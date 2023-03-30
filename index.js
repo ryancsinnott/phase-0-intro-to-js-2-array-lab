c7feae17e5e58172c42ee96dd7b4b1fddc86b4ad
Ryan Sinnott <ryan.christopher.sinnott@gmail.com>
2023-03-29 23:19:55 -0400

    Completed Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(catname){
+    console.log(catname)
+    cats.push(catname);   
+
+}
+
+function destructivelyPrependCat(name){
+    console.log(name)
+    cats.unshift(name);    
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();    
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();    
+}
+
+function appendCat(name){
+    const copyCats = [...cats];
+        copyCats.push(name);  
+        return copyCats;
+}
+
+function prependCat(name){
+    const copyCats = [...cats];
+        copyCats.unshift(name);  
+        return copyCats;
+}
+
+function removeLastCat(name){
+    const copyCats = [...cats];
+        copyCats.pop(name);  
+        return copyCats;
+}
+
+function removeFirstCat(name){
+    const copyCats = [...cats];
+        copyCats.shift(name);  
+        return copyCats;
+}
